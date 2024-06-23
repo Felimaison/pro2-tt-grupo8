b89fdcab88d83bfcccd5fffe8180c2aebdb97e00
Felimaison <142600234+Felimaison@users.noreply.github.com>
2024-06-23 18:22:59 -0300

    login: desarrolo completo + validaciones control de acceso, y modificaicones condicionales en vistas

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
+const {body} = require("express-validator");
 const perfilController = require('../controllers/perfilcontrollers');
+const db = require('../database/models');
+const bcrypt = require("bcryptjs");
+const session = require('express-session')
+
+let validacionLogin = [
+body('email')
+.notEmpty().withMessage('Debes colocar un email').bail()
+.isEmail().withMessage('El email debe ser correcto').bail()
+.custom(function(value, {req}){
+    return db.Usuario.findOne({where: { mail: req.body.email },})
+          .then(function(user){
+                if(user != undefined){ 
+                    return true;
+                }
+                else{
+                    throw new Error ('Este email no existe')
+                }
+          })
+}),
+
+body('password')
+.notEmpty().withMessage('Debes de rellenar la contraseña').bail()
+.custom(function(value, {req}){
+
+    return db.Usuario.findOne({where: { mail: req.body.email },})
+          .then(function(result){
+                if(result != undefined){ 
+
+                    let check = bcrypt.compareSync(req.body.password, result.contrasenia);
+                    if(!check){
+                        throw new Error ('La contraseña no es correcta')
+                    }
+                }
+                else{
+                    throw new Error ('Debes registrarte primero para poder acceder')
+                }
+          })
+
+})
+]
+
+
+
 
 router.get('/login', perfilController.login);
-router.post("/login", perfilController.loginUser);
+router.post("/login",validacionLogin, perfilController.loginUser);
 
 router.get('/register', perfilController.register);
 router.post('/register', perfilController.store);
