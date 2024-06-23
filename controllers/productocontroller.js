80532a8dd6104fb95efb10ed389ac9f9399592e1
theogolova <tgolova@udesa.edu.ar>
2024-06-23 18:50:19 -0300

    crear productos: desarrollo completo + validaciones y conytol de acceso

--- controllers/productocontroller.js
+++ controllers/productocontroller.js
@@ -5,9 +5,33 @@ const productContoller ={
     product: function (req, res) {
         res.render("product", {title: "Product detail", productos: zapatillas.productos})
     },
-    cargarProducto: function (req, res) {
-        res.render('productAdd', { usuario: zapatillas.usuario });
-    }
+    create: function(req, res) {
+        if(req.session.user != undefined){
+            return res.render("productAdd",{title: "Add Product"})
+        }
+        else{
+            return res.redirect("/users/login")
+        }
+        
+    },
+
+    store: function(req, res) {
+        let form = req.body;
+        let errors = validationResult(req);
+
+        if(errors.isEmpty()) {
+            
+            db.Product.create(form)
+            .then((result) => {
+                return res.redirect("/product/id/" + result.id)
+            }).catch((err) =>{
+                return console.log(err);
+            });
+        }
+        else{
+            return res.render("productAdd", {errors: errors.mapped(), old:req.body});
+        }
+    },
 }
 
 module.exports = productContoller;
--- routes/producto.js
+++ routes/producto.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
 const productContoller = require('../controllers/productocontroller');
+const { body } = require('express-validator');
 
+let validacionAgregar = [
+    body('nombreProd')
+        .notEmpty().withMessage('El producto debe tener un nombre'),
+    body('descripcion')
+        .notEmpty().withMessage('Debes agregar una descripción'),
+    body('imagenProd')
+        .notEmpty().withMessage('Debes agregar una imagen').bail()
+        .isURL().withMessage('La URL de la imagen no es valida')
+]
 router.get('/id/:id', productContoller.product);
 
 router.get('/add', productContoller.create);
-router.post('/add', productContoller.store);
+router.post('/add',validacionAgregar, productContoller.store);
 
 router.post('/editProduct', productContoller.formUpdate);
 router.post('/edit', productContoller.update);
